f28057c44dc2aed6dbb3a669c378ba2d48daa583
daswer123 <daswerq123@gmail.com>
2020-09-07 17:27:46 +0300

    Adding react and introduce with some new Redax's components

---
+++ testredux/src/actions.js
@@ -0,0 +1,15 @@
+const inc = () => {
+    return {type: "INC"}
+  }
+  
+  const dec = () => {
+    return {type: "DEC"}
+  }
+  
+  const res = () => {
+    return {type: "RES"}
+  }
+
+  export {inc};
+  export {dec};
+  export {res};
--- testredux/src/index.js
+++ testredux/src/index.js
@@ -1,55 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import {createStore} from "redux";
+import {Provider} from "react-redux";
+// import * as actions from "./actions";
+import reduce from "./reducer";
+import App from "./components/App";
 
-const reducer = (state = 0,action) =>{
+let state = createStore(reduce);
+// const {inc,dec,res} = bindActionCreators(actions,state.dispatch);
 
-  switch (action.type) {
-    case "INC" : 
-      return state + 1;
-    case "DEC" : 
-      return state - 1;
-    case "RES" :
-      return 0;
-    default:
-      return state;
-  }
+ReactDOM.render(
+    <Provider store={state}>
+            <App/>
+    </Provider>,document.getElementById("root"));
 
-}
-
-const counter = document.querySelector(".counter-info p");
-let state = createStore(reducer);
-
-state.subscribe(() =>{
-  counter.textContent = state.getState(); 
-})
-
-
-const plus = document.querySelector(".plus");
-const minus = document.querySelector(".minus");
-const reset = document.querySelector(".reset");
-
-console.log(counter,plus,minus,reset)
-
-const inc = () => {
-  return {type: "INC"}
-}
-
-const dec = () => {
-  return {type: "DEC"}
-}
-
-const res = () => {
-  return {type: "RES"}
-}
-
-
-plus.addEventListener("click", () => {
-  state.dispatch(inc());
-})
-
-minus.addEventListener("click", () => {
-  state.dispatch(dec());
-})
-
-reset.addEventListener("click", () => {
-  state.dispatch(res());
-})
+// state.subscribe(update)
